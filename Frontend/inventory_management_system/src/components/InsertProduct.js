841a9670dc936705fe8e59fd1cbf4328d5a7f924
AliShoaibHassan <alishoaib007@yahoo.com>
2025-03-25 16:41:14 +0500

    build folder for ec2 deployment

--- Frontend/inventory_management_system/src/components/InsertProduct.js
+++ Frontend/inventory_management_system/src/components/InsertProduct.js
@@ -34,7 +34,7 @@ export default function InsertProduct() {
         setError("");
 
         try {
-            const res = await fetch("http://localhost:3001/insertproduct", {
+            const res = await fetch("http://34.199.196.218:3001/insertproduct", {
                 method: "POST",
                 headers: {
                     "Content-Type": "application/json"
